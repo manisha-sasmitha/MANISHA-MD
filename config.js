24c1e2f83130257aa7b843c2198b709d3d5d6fb1
manisha Sasmitha <manishasasmitha192@gmail.com>
2024-11-15 08:57:12 +0530

    MANISHA-MD

--- config.js
+++ config.js
@@ -10,4 +10,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/6ctae3.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*🎇WELLCOME TO MANISHA-MD BOT🎇*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
